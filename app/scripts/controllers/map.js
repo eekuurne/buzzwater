d822ca94fe23b3215de4070fe4a3a3c7b9e3de80
Felix Lindholm <felix.lindholm@gmail.com>
2016-04-24 02:31:10 +0300

    enable weekends

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -163,19 +163,11 @@ angular.module('buzzwaterApp')
       };
 
       $scope.dateOptions = {
-        dateDisabled: disabled,
         formatYear: 'yy',
         maxDate: new Date(2020, 5, 22),
         minDate: new Date(),
         startingDay: 1
       };
-
-      // Disable weekend selection
-      function disabled(data) {
-        var date = data.date,
-          mode = data.mode;
-        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-      }
 
       $scope.toggleMin = function() {
         $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
